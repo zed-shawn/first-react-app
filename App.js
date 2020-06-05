f67cd100436477b186ae2a052fe34597471b8299
zed.shawn <alizeeshan1999@gmail.com>
2020-06-05 14:22:12 +0530

    modified:   App.js

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText]= useState('Open up App.js to start working on your app!');
+
+  const toggle = () =>{
+    setOutputText('button has been pressed');
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title='Change text'/>
+      <Text>{outputText}</Text>
+      <Button title='Change text' onPress={() => toggle()}/>
     </View>
   );
 }
